110eeb94d4f81599a5605344ee9d32ca9a3fed18
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-07-08 15:13:20 +0200

    created StudentMongoRepository

---
+++ com.examples.school/src/main/java/com/examples/school/repository/mongo/StudentMongoRepository.java
@@ -0,0 +1,12 @@
+package com.examples.school.repository.mongo;
+
+import com.mongodb.MongoClient;
+
+public class StudentMongoRepository {
+
+	public StudentMongoRepository(MongoClient mongoClient,
+			String schoolDbName, String studentCollectionName) {
+
+	}
+
+}
