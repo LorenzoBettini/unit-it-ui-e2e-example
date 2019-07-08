5aa22965ef230ae74835291d56f5744288288f84
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-07-08 15:19:27 +0200

    initial implemnetation of findAll

--- com.examples.school/src/main/java/com/examples/school/repository/mongo/StudentMongoRepository.java
+++ com.examples.school/src/main/java/com/examples/school/repository/mongo/StudentMongoRepository.java
@@ -1,12 +1,34 @@
 package com.examples.school.repository.mongo;
 
+import java.util.Collections;
+import java.util.List;
+
+import com.examples.school.model.Student;
+import com.examples.school.repository.StudentRepository;
 import com.mongodb.MongoClient;
 
-public class StudentMongoRepository {
+public class StudentMongoRepository implements StudentRepository {
 
 	public StudentMongoRepository(MongoClient mongoClient,
 			String schoolDbName, String studentCollectionName) {
 
+	}
+
+	@Override
+	public List<Student> findAll() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public Student findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Student student) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
