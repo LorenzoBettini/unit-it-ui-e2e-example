541908d4909fbfeff7e518cdabf810195e99ad88
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-07-08 14:07:23 +0200

    failing test for allStudents

--- com.examples.school/src/test/java/com/examples/school/controller/SchoolControllerTest.java
+++ com.examples.school/src/test/java/com/examples/school/controller/SchoolControllerTest.java
@@ -1,10 +1,17 @@
 package com.examples.school.controller;
 
+import static java.util.Arrays.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.examples.school.model.Student;
 import com.examples.school.repository.StudentRepository;
 import com.examples.school.view.StudentView;
 
@@ -22,5 +29,15 @@ public class SchoolControllerTest {
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testAllStudents() {
+		List<Student> students = asList(new Student());
+		when(studentRepository.findAll())
+			.thenReturn(students);
+		schoolController.allStudents();
+		verify(studentView)
+			.showAllStudents(students);
 	}
 }
