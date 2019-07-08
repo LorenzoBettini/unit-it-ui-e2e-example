a9c32b421b6f684a00dca83837526804a2d690f7
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-07-08 15:31:51 +0200

    implemented findAll

--- com.examples.school/src/main/java/com/examples/school/repository/mongo/StudentMongoRepository.java
+++ com.examples.school/src/main/java/com/examples/school/repository/mongo/StudentMongoRepository.java
@@ -1,22 +1,32 @@
 package com.examples.school.repository.mongo;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.bson.Document;
 
 import com.examples.school.model.Student;
 import com.examples.school.repository.StudentRepository;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 
 public class StudentMongoRepository implements StudentRepository {
 
+	private MongoCollection<Document> studentCollection;
+
 	public StudentMongoRepository(MongoClient mongoClient,
 			String schoolDbName, String studentCollectionName) {
-
+		studentCollection = mongoClient
+			.getDatabase(schoolDbName)
+			.getCollection(studentCollectionName);
 	}
 
 	@Override
 	public List<Student> findAll() {
-		return Collections.emptyList();
+		return StreamSupport.stream(studentCollection.find().spliterator(), false)
+			.map(d -> new Student(d.get("id")+"", d.get("name")+""))
+			.collect(Collectors.toList());
 	}
 
 	@Override
