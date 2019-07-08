16950938fb4bf69e7424c84529df0dc4d8ddc479
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-07-08 14:10:31 +0200

    implemented allStudents

--- com.examples.school/src/main/java/com/examples/school/controller/SchoolController.java
+++ com.examples.school/src/main/java/com/examples/school/controller/SchoolController.java
@@ -1,5 +1,20 @@
 package com.examples.school.controller;
 
+import com.examples.school.repository.StudentRepository;
+import com.examples.school.view.StudentView;
+
 public class SchoolController {
+
+	private StudentView studentView;
+	private StudentRepository studentRepository;
+
+	public SchoolController(StudentView studentView, StudentRepository studentRepository) {
+		this.studentView = studentView;
+		this.studentRepository = studentRepository;
+	}
+
+	public void allStudents() {
+		studentView.showAllStudents(studentRepository.findAll());
+	}
 
 }
