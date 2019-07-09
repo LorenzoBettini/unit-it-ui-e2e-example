94ac3c99ec88ce182be770724913f37252daf014
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-07-09 12:32:11 +0200

    implemented all controls

--- com.examples.school/src/main/java/com/examples/school/view/swing/StudentSwingView.java
+++ com.examples.school/src/main/java/com/examples/school/view/swing/StudentSwingView.java
@@ -3,26 +3,41 @@ package com.examples.school.view.swing;
 import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class StudentSwingView extends JFrame {
+import com.examples.school.model.Student;
+import com.examples.school.view.StudentView;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import java.awt.Color;
+
+public class StudentSwingView extends JFrame implements StudentView {
 
 	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField txtId;
+	private JTextField txtName;
+	private JButton btnAdd;
+	private JList listStudents;
+	private JScrollPane scrollPane;
+	private JLabel lblErrorMessage;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				try {
 					StudentSwingView frame = new StudentSwingView();
@@ -38,6 +53,7 @@ public class StudentSwingView extends JFrame {
 	 * Create the frame.
 	 */
 	public StudentSwingView() {
+		setTitle("Student View");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -45,27 +61,97 @@ public class StudentSwingView extends JFrame {
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
 		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JLabel lblId = new JLabel("id");
 		GridBagConstraints gbc_lblId = new GridBagConstraints();
-		gbc_lblId.insets = new Insets(0, 0, 0, 5);
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
 		gbc_lblId.anchor = GridBagConstraints.EAST;
 		gbc_lblId.gridx = 0;
 		gbc_lblId.gridy = 0;
 		contentPane.add(lblId, gbc_lblId);
 		
-		textField = new JTextField();
-		textField.setName("idTextBox");
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 0;
-		contentPane.add(textField, gbc_textField);
-		textField.setColumns(10);
+		txtId = new JTextField();
+		txtId.setName("idTextBox");
+		GridBagConstraints gbc_idTextField = new GridBagConstraints();
+		gbc_idTextField.insets = new Insets(0, 0, 5, 0);
+		gbc_idTextField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_idTextField.gridx = 1;
+		gbc_idTextField.gridy = 0;
+		contentPane.add(txtId, gbc_idTextField);
+		txtId.setColumns(10);
+		
+		JLabel lblName = new JLabel("name");
+		GridBagConstraints gbc_lblName = new GridBagConstraints();
+		gbc_lblName.anchor = GridBagConstraints.EAST;
+		gbc_lblName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblName.gridx = 0;
+		gbc_lblName.gridy = 1;
+		contentPane.add(lblName, gbc_lblName);
+		
+		txtName = new JTextField();
+		txtName.setName("nameTextBox");
+		GridBagConstraints gbc_nameTextField = new GridBagConstraints();
+		gbc_nameTextField.insets = new Insets(0, 0, 5, 0);
+		gbc_nameTextField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_nameTextField.gridx = 1;
+		gbc_nameTextField.gridy = 1;
+		contentPane.add(txtName, gbc_nameTextField);
+		txtName.setColumns(10);
+		
+		btnAdd = new JButton("Add");
+		btnAdd.setEnabled(false);
+		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
+		gbc_btnAdd.insets = new Insets(0, 0, 5, 0);
+		gbc_btnAdd.gridwidth = 2;
+		gbc_btnAdd.gridx = 0;
+		gbc_btnAdd.gridy = 2;
+		contentPane.add(btnAdd, gbc_btnAdd);
+		
+		scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridwidth = 2;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 3;
+		contentPane.add(scrollPane, gbc_scrollPane);
+		
+		listStudents = new JList();
+		listStudents.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		listStudents.setName("studentList");
+		scrollPane.setViewportView(listStudents);
+		
+		lblErrorMessage = new JLabel(" ");
+		lblErrorMessage.setForeground(Color.RED);
+		lblErrorMessage.setName("errorMessageLabel");
+		GridBagConstraints gbc_lblErrorMessage = new GridBagConstraints();
+		gbc_lblErrorMessage.gridwidth = 2;
+		gbc_lblErrorMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_lblErrorMessage.gridx = 0;
+		gbc_lblErrorMessage.gridy = 5;
+		contentPane.add(lblErrorMessage, gbc_lblErrorMessage);
+	}
+
+	@Override
+	public void showAllStudents(List<Student> students) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showError(String message, Student student) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void studentAdded(Student student) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
